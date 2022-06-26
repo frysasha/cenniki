2cc8be9baa3866c8976b15d82fbf05e9c2a82ff5
frysasha <frysasha@yandex.ru>
2022-06-26 18:53:19 +0300

    business logic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import datetime
-
 from reportlab.graphics.shapes import Drawing
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.platypus import SimpleDocTemplate
@@ -16,6 +15,14 @@ import csv
 CENNIK_WIDTH = 7 * cm
 CENNIK_WIDTH_COUNT = 4
 
+fileName = 'cenniki.pdf'
+
+pdf = SimpleDocTemplate(fileName, pagesize=landscape(A4), topMargin=0 * cm, bottommargin=0 * cm)
+
+pdfmetrics.registerFont(TTFont('Roboto-Medium', 'fonts/Roboto-Medium.ttf'))
+pdfmetrics.registerFont(TTFont('Roboto-Regular', 'fonts/Roboto-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('Roboto-Bold', 'fonts/Roboto-Bold.ttf'))
+
 
 class Cennik:
     title = ''
@@ -29,7 +36,7 @@ class Cennik:
     date = ''
 
 
-def getCennik(title, code, country, article, brand, price):
+def create_cennik(title, code, country, article, brand, price):
     styles = getSampleStyleSheet()
     styles.add(ParagraphStyle(name='cennik_title', wordWrap=True, fontName='Roboto-Medium', fontSize=7,
                               leading=9))  # стиль параграфа (автоматом переносит строку) leading = отступ между строками
@@ -47,52 +54,41 @@ def getCennik(title, code, country, article, brand, price):
     return cennik
 
 
-fileName = 'pdfTable.pdf'
-
-pdf = SimpleDocTemplate(fileName, pagesize=landscape(A4), topMargin=1*cm, bottommargin=1*cm)
-
-pdfmetrics.registerFont(TTFont('Roboto-Medium', 'fonts/Roboto-Medium.ttf'))
-pdfmetrics.registerFont(TTFont('Roboto-Regular', 'fonts/Roboto-Regular.ttf'))
-pdfmetrics.registerFont(TTFont('Roboto-Bold', 'fonts/Roboto-Bold.ttf'))
-
-
-def getCennikTable(cennik):
-    titleTable = Table([
+def get_cennik_table(cennik):
+    title_table = Table([
         [cennik.title]
     ], CENNIK_WIDTH, 0.65 * cm)
 
-    codeTable = Table([
+    code_table = Table([
         ['Код: ', cennik.code]
     ])
 
-    articleTable = Table([
+    article_table = Table([
         ['Артикул: ', cennik.article]
     ])
 
-    countryTable = Table([
+    country_table = Table([
         ['Страна: ', cennik.country]
     ])
 
-    brandTable = Table([
+    brand_table = Table([
         ['Производитель: ', cennik.brand]
     ])
 
     code_article_country_brand_table = Table([
-        [[codeTable], [articleTable]],
-        [[countryTable], [brandTable]]
+        [[code_table], [article_table]],
+        [[country_table], [brand_table]]
     ], [60])
 
     price_table = Table([[cennik.price, r'₽/шт.']])
 
     barcode_table = Table([[code128.Code128(cennik.barcode, barWidth=0.8, barHeight=12, quiet=False)]])
 
-    dateTable = Table([[cennik.date]], [60])
-
+    date_table = Table([[cennik.date]], [60])
 
     barcode_date_table = Table([
         [[barcode_table],
-         [dateTable]],
-
+         [date_table]],
     ])
 
     qrcode = QrCodeWidget(cennik.qrcode)
@@ -102,36 +98,31 @@ def getCennikTable(cennik):
     qrcode_url_draw = Drawing(50, 50)
     qrcode_url_draw.add(qrcode)
 
-    qrTable = Table([[qrcode_url_draw]])
+    qr_code_table = Table([[qrcode_url_draw]])
 
-    cennikTable = Table([
-        [titleTable],
+    cennik_table = Table([
+        [title_table],
         [code_article_country_brand_table],
         [price_table],
         [barcode_date_table],
-        [qrTable],
+        [qr_code_table],
     ], CENNIK_WIDTH, rowHeights=(0.65 * cm, 0.95 * cm, 0.65 * cm, 0.75 * cm, 0.05 * cm))  # расположение по очереди
 
-    titleTableStyle = TableStyle([
+    title_table_style = TableStyle([
         ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
         ('FONTSIZE', (0, 0), (-1, -1), 7),
         ('FONTNAME', (0, 0), (-1, -1), 'Roboto-Medium'),
         ('TOPPADDING', (0, 0), (-1, -1), 0),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
-        # ('BOX', (0, 0), (0, 0), 1, colors.black),
     ])
-    titleTable.setStyle(titleTableStyle)
+    title_table.setStyle(title_table_style)
 
-    codeTableStyle = TableStyle([
+    code_table_style = TableStyle([
         ('FONTSIZE', (0, 0), (0, 0), 6),
         ('FONTSIZE', (1, 0), (1, 0), 9),
         ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-        ('FONTNAME', (0, 0), (0, 0),
-         'Roboto-Regular'
-         ),
-        ('FONTNAME', (1, 0), (1, 0),
-         'Roboto-Medium'
-         ),
+        ('FONTNAME', (0, 0), (0, 0), 'Roboto-Regular'),
+        ('FONTNAME', (1, 0), (1, 0), 'Roboto-Medium'),
         ('FONTSIZE', (0, 0), (-1, -1), 6),
         ('FONTSIZE', (1, 0), (-1, -1), 9),
         ('TOPPADDING', (0, 0), (-1, -1), 0),
@@ -139,53 +130,41 @@ def getCennikTable(cennik):
         ('RIGHTPADDING', (0, 0), (-1, -1), 0),
         ('LEFTPADDING', (0, 0), (-1, -1), 1),
     ])
-    codeTable.setStyle(codeTableStyle)
+    code_table.setStyle(code_table_style)
 
-    countryTableStyle = TableStyle([
+    country_table_style = TableStyle([
         ('TOPPADDING', (0, 0), (-1, -1), 0),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
-        ('FONTNAME', (0, 0), (0, 0),
-         'Roboto-Regular'
-         ),
-        ('FONTNAME', (-1, -1), (-1, -1),
-         'Roboto-Medium'
-         ),
+        ('FONTNAME', (0, 0), (0, 0), 'Roboto-Regular'),
+        ('FONTNAME', (-1, -1), (-1, -1), 'Roboto-Medium'),
         ('FONTSIZE', (0, 0), (-1, -1), 6),
         ('RIGHTPADDING', (0, 0), (-1, -1), 0),
         ('LEFTPADDING', (0, 0), (-1, -1), 1),
     ])
-    countryTable.setStyle(countryTableStyle)
+    country_table.setStyle(country_table_style)
 
-    articleTableStyle = TableStyle([
+    article_table_style = TableStyle([
         ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
-        ('FONTNAME', (0, 0), (0, 0),
-         'Roboto-Regular'
-         ),
-        ('FONTNAME', (-1, -1), (-1, -1),
-         'Roboto-Medium'
-         ),
+        ('FONTNAME', (0, 0), (0, 0), 'Roboto-Regular'),
+        ('FONTNAME', (-1, -1), (-1, -1), 'Roboto-Medium'),
         ('FONTSIZE', (0, 0), (-1, -1), 6),
         ('TOPPADDING', (0, 0), (-1, -1), 0),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
         ('RIGHTPADDING', (0, 0), (-1, -1), 0),
         ('LEFTPADDING', (0, 0), (-1, -1), 1),
     ])
-    articleTable.setStyle(articleTableStyle)
+    article_table.setStyle(article_table_style)
 
-    brandTableStyle = TableStyle([
+    brand_table_style = TableStyle([
         ('TOPPADDING', (0, 0), (-1, -1), 0),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
-        ('FONTNAME', (0, 0), (0, 0),
-         'Roboto-Regular'
-         ),
-        ('FONTNAME', (-1, -1), (-1, -1),
-         'Roboto-Medium'
-         ),
+        ('FONTNAME', (0, 0), (0, 0), 'Roboto-Regular'),
+        ('FONTNAME', (-1, -1), (-1, -1), 'Roboto-Medium'),
         ('FONTSIZE', (0, 0), (-1, -1), 6),
         ('RIGHTPADDING', (0, 0), (-1, -1), 0),
         ('LEFTPADDING', (0, 0), (-1, -1), 1),
     ])
-    brandTable.setStyle(brandTableStyle)
+    brand_table.setStyle(brand_table_style)
 
     code_article_country_brand_table_style = TableStyle([
 
@@ -201,14 +180,10 @@ def getCennikTable(cennik):
         ('BOTTOMPADDING', (0, 0), (0, 0), 7),
         ('BOTTOMPADDING', (-1, -1), (-1, -1), 0),
         ('VALIGN', (1, 0), (1, 0), 'BOTTOM'),
-        ('FONTNAME', (0, 0), (0, 0),
-         'Roboto-Bold'
-         ),
+        ('FONTNAME', (0, 0), (0, 0), 'Roboto-Bold'),
         ('FONTSIZE', (0, 0), (0, 0), 15),
         ('FONTSIZE', (-1, -1), (-1, -1), 8),
-        ('FONTNAME', (-1, -1), (-1, -1),
-         'Roboto-Regular'
-         ),
+        ('FONTNAME', (-1, -1), (-1, -1), 'Roboto-Regular'),
 
     ])
     price_table.setStyle(price_table_style)
@@ -220,37 +195,33 @@ def getCennikTable(cennik):
     ])
     barcode_table.setStyle(barcode_table_style)
 
-    dateTableStyle = TableStyle([
+    date_table_style = TableStyle([
         ('TOPPADDING', (0, 0), (-1, -1), 0),
         ('LEFTPADDING', (0, 0), (-1, -1), 0),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 2),
-        # ('BOX', (0, 0), (-1, -1), .5, colors.black),
-        ('FONTNAME', (-1, -1), (-1, -1),
-         'Roboto-Regular'
-         ),
+        ('FONTNAME', (-1, -1), (-1, -1), 'Roboto-Regular'),
         ('FONTSIZE', (0, 0), (-1, -1), 4),
         ('VALIGN', (0, 0), (-1, -1), 'BOTTOM'),
     ])
-    dateTable.setStyle(dateTableStyle)
+    date_table.setStyle(date_table_style)
 
     barcode_data_table_style = TableStyle([
 
         ('LEFTPADDING', (0, 0), (-1, -1), 0),
         ('TOPPADDING', (0, 0), (-1, -1), 0),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
-        # ('INNERGRID', (0, 0), (-1, -1), .5, colors.red),
     ])
 
     barcode_date_table.setStyle(barcode_data_table_style)
 
-    qrTableStyle = TableStyle([
+    qr_code_table_style = TableStyle([
         ('LEFTPADDING', (0, 0), (-1, -1), 4.7 * cm),
         ('BOTTOPPADDING', (0, 0), (-1, -1), 0),
 
     ])
-    qrTable.setStyle(qrTableStyle)
+    qr_code_table.setStyle(qr_code_table_style)
 
-    cennikTableStyle = TableStyle([
+    cennik_table_style = TableStyle([
         ('BOX', (0, 0), (-1, -1), 0.5, colors.black),
         ('LINEABOVE', (0, 1), (0, 1), 0.5, colors.black),
         ('TOPPADDING', (0, 0), (-1, -1), 0),
@@ -258,12 +229,12 @@ def getCennikTable(cennik):
         ('RIGHTPADDING', (0, 0), (-1, -1), 0),
         ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
     ])
-    cennikTable.setStyle(cennikTableStyle)
+    cennik_table.setStyle(cennik_table_style)
 
-    return cennikTable
+    return cennik_table
 
 
-def open_csv(filename):
+def get_cennik_data(filename):
     with open(filename) as csv_file:
         csvdata = csv.reader(csv_file, delimiter=';')
         cennik_table_data = []
@@ -284,45 +255,48 @@ def open_csv(filename):
     return cennik_table_data
 
 
-c = open_csv('cena1.csv')[0]
-c1 = getCennikTable(
-    getCennik(title=c['name'], code=c['code'], article=c['article'], brand=c['brand'], country=c['brand'],
-              price=c['price']))
+def get_cennik(cennik_data):
+    all_rows_table = []  # все ценники
+    row_table = []  # по 4 ценника в строке
 
-all_rows_table = []  # все ценники
+    for c in cennik_data:
+        if len(row_table) >= 4:
+            all_rows_table.append(row_table)
+            row_table = []
+            row_table.append(get_cennik_table(
+                create_cennik(title=c['name'], code=c['code'], article=c['article'], brand=c['brand'],
+                              country=c['brand'],
+                              price=c['price'])))
+        else:
+            row_table.append(get_cennik_table(
+                create_cennik(title=c['name'], code=c['code'], article=c['article'], brand=c['brand'],
+                              country=c['brand'],
+                              price=c['price'])))
 
-row_table = []  # по 4 ценника в строке
-for c in open_csv('cena1.csv'):
-    if len(row_table) >= 4:
-        all_rows_table.append(row_table)
-        row_table = []
-        row_table.append(getCennikTable(
-            getCennik(title=c['name'], code=c['code'], article=c['article'], brand=c['brand'], country=c['brand'],
-                      price=c['price'])))
-    else:
-        row_table.append(getCennikTable(
-            getCennik(title=c['name'], code=c['code'], article=c['article'], brand=c['brand'], country=c['brand'],
-                      price=c['price'])))
+    all_rows_table.append(row_table)
+
+    return all_rows_table
 
 
-all_rows_table.append(row_table)
+def build_main_table(cennik_table):
+    main_table = Table(cennik_table)
 
-mainTable = Table(all_rows_table)
-print(len(open_csv('cena1.csv')))
-print(len(row_table))
-print(len(all_rows_table))
+    main_table_style = TableStyle([
+        ('TOPPADDING', (0, 0), (-1, -1), 1),
+        ('LEFTPADDING', (0, 0), (-1, -1), 1),
+        ('RIGHTPADDING', (0, 0), (-1, -1), 1),
+        ('BOTTOMPADDING', (0, 0), (-1, -1), 1),
+    ])
+    main_table.setStyle(main_table_style)
 
-mainTableStyle = TableStyle([
-    ('TOPPADDING', (0, 0), (-1, -1), 1),
-    ('LEFTPADDING', (0, 0), (-1, -1), 1),
-    ('RIGHTPADDING', (0, 0), (-1, -1), 1),
-    ('BOTTOMPADDING', (0, 0), (-1, -1), 1),
-])
-mainTable.setStyle(mainTableStyle)
+    pdf.build([main_table])
 
-elems = []
-elems.append(mainTable)
-pdf.build(elems)
+
+def build_pdf(csv_file):
+    cennik_data = get_cennik_data(csv_file)
+    cennik_table = get_cennik(cennik_data)
+    build_main_table(cennik_table)
+
 
 if __name__ == '__main__':
-    pass
+    build_pdf('cena1.csv')
